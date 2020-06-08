e511d26e137f90cf14e503e622b14f5e6801a7d0
Miklos Herald Narra <miklos.herald@gmail.com>
2020-06-08 05:53:03 -0700

    commit iOS

---
+++ ctydelivery/ctydelivery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ctydelivery
+//
+//  Created by Miklos on 8/6/20.
+//  Copyright © 2020 Miklos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
